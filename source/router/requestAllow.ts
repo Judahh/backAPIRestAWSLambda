353a52dd9e22f3a3669cab60bc68beebd50ef495
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-06-29 14:24:27 -0300

    fix

--- source/router/requestAllow.ts
+++ source/router/requestAllow.ts
@@ -1,6 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-const requestAllow = async (req: NextApiRequest, res: NextApiResponse) => {
+const requestAllow = async (req, res) => {
   const { method } = req;
   res['method'] = method + 'Allow';
 };
