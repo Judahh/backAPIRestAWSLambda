334691936464fc26dcf5e66f97d2d2537ccf7d24
Restyled.io <commits@restyled.io>
2022-06-30 12:44:53 +0000

    Restyled by shellharden

--- script/backAWS.sh
+++ script/backAWS.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env sh
 while getopts p:db flag
 do
-    case "${flag}" in
-        p) port=${OPTARG};;
+    case "$flag" in
+        p) port=$OPTARG;;
         d) exec="dev";;
         b) exec="build";;
     esac
 done
 
-pwd=$(pwd)
-cd $pwd;
+pwd=$PWD
+cd "$pwd";
 
 if [ -f ".env" ]; then
     echo ".env exists."
     cat .env | grep -v '#' | grep PORT
-    export $(cat .env | grep -v '#' | grep PORT)
+    export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
 
-echo "setted Port: ${port}"
+echo "setted Port: $port"
 
 case $exec in
   "dev")
-    (cd $pwd ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
+    (cd "$pwd" ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
     ;;
 
   "build")
-    (cd $pwd ; npm run --prefix $pwd tsc ; backBuild)
+    (cd "$pwd" ; npm run --prefix "$pwd" tsc ; backBuild)
     ;;
 
   "")
-    sam local start-api -p $port
+    sam local start-api -p "$port"
     ;;
 esac
