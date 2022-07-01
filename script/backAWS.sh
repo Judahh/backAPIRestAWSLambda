969a3de05aa490e2195f3e00db458e38cdccaab2
Restyled.io <commits@restyled.io>
2022-07-01 19:27:56 +0000

    Restyled by shellharden

--- script/backAWS.sh
+++ script/backAWS.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env sh
 while getopts p:db flag
 do
-    case "${flag}" in
-        p) port=${OPTARG};;
+    case "$flag" in
+        p) port=$OPTARG;;
         d) exec="dev";;
         b) exec="build";;
     esac
 done
 
-pwd=$(pwd)
-cd $pwd;
+pwd=$PWD
+cd "$pwd";
 
 if [ -f ".env" ]; then
     echo ".env exists."
     cat .env | grep -v '#' | grep PORT
-    export $(cat .env | grep -v '#' | grep PORT)
+    export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
 
-echo "setted Port: ${port}"
+echo "setted Port: $port"
 
 case $exec in
   "dev")
-    (cd $pwd ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
+    (cd "$pwd" ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
     ;;
 
   "build")
-    (cd $pwd; rm -rf dist/; backAWSBuild; webpack; cp ./package.json ./dist/package.json)
+    (cd "$pwd"; rm -rf dist/; backAWSBuild; webpack; cp ./package.json ./dist/package.json)
     ;;
 
   "full-build")
-    (cd $pwd; rm -rf dist/; backAWSBuild; webpack; cp ./package.json ./dist/package.json; sam-build)
+    (cd "$pwd"; rm -rf dist/; backAWSBuild; webpack; cp ./package.json ./dist/package.json; sam-build)
     ;;
 
   "")
-    sam local start-api -p $port
+    sam local start-api -p "$port"
     ;;
 esac
