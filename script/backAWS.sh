01a5ed80cf90afad45882fa820c65119c60f8d9c
Restyled.io <commits@restyled.io>
2022-07-01 12:02:41 +0000

    Restyled by shfmt

--- script/backAWS.sh
+++ script/backAWS.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
-while getopts p:db flag
-do
-    case "$flag" in
-        p) port=$OPTARG;;
-        d) exec="dev";;
-        b) exec="build";;
-    esac
+while getopts p:db flag; do
+  case "$flag" in
+    p) port=$OPTARG ;;
+    d) exec="dev" ;;
+    b) exec="build" ;;
+  esac
 done
 
 pwd=$PWD
-cd "$pwd";
+cd "$pwd"
 
 if [ -f ".env" ]; then
-    echo ".env exists."
-    cat .env | grep -v '#' | grep PORT
-    export "$(cat .env | grep -v '#' | grep PORT)"
+  echo ".env exists."
+  cat .env | grep -v '#' | grep PORT
+  export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
@@ -23,11 +22,18 @@ echo "setted Port: $port"
 
 case $exec in
   "dev")
-    (cd "$pwd" ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
+    (
+      cd "$pwd"
+      ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start"
+    )
     ;;
 
   "build")
-    (cd "$pwd" ;  backBuild; npm run --prefix "$pwd" webpack ;)
+    (
+      cd "$pwd"
+      backBuild
+      npm run --prefix "$pwd" webpack
+    )
     ;;
 
   "")
