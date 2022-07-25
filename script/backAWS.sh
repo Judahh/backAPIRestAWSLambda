d4ce2c4f24b82873c9214cefbbe21c9a2da963df
Restyled.io <commits@restyled.io>
2022-07-25 20:02:27 +0000

    Restyled by shfmt

--- script/backAWS.sh
+++ script/backAWS.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
-while getopts p:dmb flag
-do
-    case "$flag" in
-        p) port=$OPTARG;;
-        d) exec="dev";;
-        m) exec="migrate";;
-        b) exec="build";;
-    esac
+while getopts p:dmb flag; do
+  case "$flag" in
+    p) port=$OPTARG ;;
+    d) exec="dev" ;;
+    m) exec="migrate" ;;
+    b) exec="build" ;;
+  esac
 done
 
 dist="${npm_package_config_path_dist:-.}"
@@ -14,12 +13,12 @@ server="${npm_package_config_path_server:-source/server.js}"
 file=$dist/$server
 
 pwd=$PWD
-cd "$pwd";
+cd "$pwd"
 
 if [ -f ".env" ]; then
-    echo ".env exists."
-    cat .env | grep -v '#' | grep PORT
-    export "$(cat .env | grep -v '#' | grep PORT)"
+  echo ".env exists."
+  cat .env | grep -v '#' | grep PORT
+  export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 port="${PORT:=3000}"
@@ -28,23 +27,43 @@ echo "setted Port: $port"
 
 case $exec in
   "dev")
-    (cd "$pwd" ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
+    (
+      cd "$pwd"
+      ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start"
+    )
     ;;
 
   "build")
-    (cd "$pwd"; rm -rf dist/; backAWSBuild; webpack; cp ./package.json ./dist/package.json)
+    (
+      cd "$pwd"
+      rm -rf dist/
+      backAWSBuild
+      webpack
+      cp ./package.json ./dist/package.json
+    )
     ;;
 
   "full-build")
-    (cd "$pwd"; rm -rf dist/; backAWSBuild; webpack; cp ./package.json ./dist/package.json; sam-build)
+    (
+      cd "$pwd"
+      rm -rf dist/
+      backAWSBuild
+      webpack
+      cp ./package.json ./dist/package.json
+      sam-build
+    )
     ;;
 
   "migrate")
     if test -f "$file"; then
       (node "$file" -m)
     else
-      (npm tsc & node ./node_modules/@backapirest/aws-lambda/script/migrate.mjs -f "$pwd")
-    fi;;
+      (
+        npm tsc &
+        node ./node_modules/@backapirest/aws-lambda/script/migrate.mjs -f "$pwd"
+      )
+    fi
+    ;;
 
   "")
     sam local start-api -p "$port"
